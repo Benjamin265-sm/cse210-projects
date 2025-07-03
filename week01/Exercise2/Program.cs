3e19fb075d1442ed4b8c510b039f356983f36652
Benjamin265-sm <you@example.com>
2025-07-03 10:31:55 -0700

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = grade.ToUpper();
+        if (number >= 90)
+        {
+
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You have passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the course, try again next time.");
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+
     }
 }
